72fa78ed94bf60e388eb121c5b30be923f01ddbe
Abdulmajid Adesokan <abdulmajidadesokan@Abdulmajids-MacBook-Pro.local>
2022-12-06 20:11:43 +0100

    check the host

--- azure-vote/azure-vote/main.py
+++ azure-vote/azure-vote/main.py
@@ -87,4 +87,4 @@ def index():
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
 if __name__ == "__main__":
-    app.run(port=8000)
+    app.run(port=8000,host="0.0.0.0")
